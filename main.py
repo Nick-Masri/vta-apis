b5db6200b2a5bdd73a807860c99e63026c3e919e
Nick Masri <nicholasmasri@gmail.com>
2023-09-29 19:41:09 -0400

    fixed requirements

--- main.py
+++ main.py
@@ -4,7 +4,4 @@ st.write("# Welcome to this app showing all the different VTA API's")
 
 st.write("Currently we have pages on:")
 st.write("1. Chargepoint")
-st.write("2. Swiftly")
-
-
-
+st.info("Future pages can include: swiftly & proterra")
